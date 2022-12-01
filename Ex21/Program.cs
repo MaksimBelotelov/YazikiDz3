f2d98dc08564c1154b7d02fb7eaa1aa82fa9a7df
Maksim <maksim_belotelov@mail.ru>
2022-12-01 23:26:22 +0300

    add Ex21

---
+++ Ex21/Program.cs
@@ -0,0 +1,23 @@
+﻿// Программа принимает координаты двух точек и находит расстояние между ними в 3D пространстве
+
+Console.Clear();
+
+Console.WriteLine("Ввод координат точки А: ");
+Console.Write("Введите координату Х: ");
+int ax=int.Parse(Console.ReadLine());
+Console.Write("Введите координату Y: ");
+int ay=int.Parse(Console.ReadLine());
+Console.Write("Введите координату Z: ");
+int az=int.Parse(Console.ReadLine());
+
+Console.WriteLine("Ввод координат точки B: ");
+Console.Write("Введите координату Х: ");
+int bx=int.Parse(Console.ReadLine());
+Console.Write("Введите координату Y: ");
+int by=int.Parse(Console.ReadLine());
+Console.Write("Введите координату Z: ");
+int bz=int.Parse(Console.ReadLine());
+
+double s=Math.Sqrt(Math.Pow(bx-ax,2)+Math.Pow(by-ay,2)+Math.Pow(bz-az,2));
+
+Console.WriteLine($"Расстояние между точками равно {s}");
